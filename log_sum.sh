6db14668bfa9ed90f6572c2c10957b09a3f6c127
elal17 <elal17@student.bth.se>
2020-09-07 13:41:23 +0200

    -e flag implemented

--- log_sum.sh
+++ log_sum.sh
@@ -1,98 +1,163 @@
 #!/bin/bash
+blacklistPrint()
+{
+	dig -f dns.blacklist.txt +short > dnsFile.txt
+	grep -f dnsFile.txt blacklistCheck > tempIP
+	#make conditions, if -F or -r flag exist, the IP address will be placed in the second column.
+	if [ $oddFlag -eq 1 ]; then
+		join blacklistCheck tempIP | awk '{ if (($2==$3)) printf("%s\t %s\t %s\n", $1, $2, "*blacklisteed*"); else printf("%s\t %s\n", $1, $2) }' > reslutFile
+	else
+		join blacklistCheck tempIP | awk '{ if (($1==$3)) printf("%s\t %s\t %s\n", $1, $2, "*blacklisteed*"); else printf("%s\t %s\n", $1, $2) }' > reslutFile
+	fi
+	cat reslutFile
+	rm dnsFile.txt
+	rm tempIP
+	rm blacklistCheck
+	rm reslutFile
+}
 
-USAGE="usage: $0 [-n N] (-c|-2|-r|-F|-t) <filename>"
 
 printResult() {
-if [ $nVal -gt 0 ]; then
+if [[ $numberOfResults -gt 0 ]]; then
 	command=$@
-	command+=" | head -n $nVal"
-	eval $command
+	if [ $eFlag -eq 1 ]; then
+		#resolve domain addresses.
+		command+=" | head -n $numberOfResults"
+		eval $command > blacklistCheck
+		blacklistPrint $command
+
+	else
+		command+=" | head -n $numberOfResults"
+		eval $command
+	fi
+elif [ $eFlag -eq 1 ]; then
+	#resolve addresses
+	eval $command >blacklistCheck
+	blacklistPrint $command
 else
 	eval $@
 fi
 exit 0
 }
 
+# Showing proper script usage to user
+USAGE="Usage: $0 [-n N] (-c|-2|-r|-F|-t) [-e] <filename>"
 
-nrOfFlags=0
-nVal=0
-nFlag=0
-file=0
+if [ $# -lt 1 ] || [ $# -gt 5 ]; then
+	echo $USAGE
+	exit 1
+fi
 
-declare -a flags
-index=0
-for arg in $@
+# Iterating through all parameters - inserting into params array
+
+declare -a params
+for param in "$@"
 do
-    re='^[0-9]+$'
-    printf "%s" $arg | grep -q "\-n"
-    if [ $? -eq 0 ]; then
-        #flag N exists
-        nFlag=1
-        index=$((index+1))
-        #nVal=$(echo ${flags[index]} | grep -o [1-9][0-9]*)  -z $nVal
-        nVal=$(echo $@ | grep -o "^[1-9][0-9]*")
-        if [ -z $nVal ]; then     #if the n-flag is not followed by a number, if nVal is empty
-            echo "flag uncomplete"
-            exit 1
-        fi
-        #flags[$index]=$arg
-        index=$((index+1))
-    elif [ -f $arg ]; then
-        file=1
-        filename=$arg
-        #echo "$arg exist"
-    elif ! [[ $arg =~ $re ]]; then #if the argument is not a number
-    #else
-        flags[$index]=$arg
-        index=$((index+1))
-    fi
-    
+    params+=($param)
 done
 
+# Checking if a file with the given name exists
 
-#check how many parameters, and if the N flag is there.
-nrOfFlags=index-1
-if [ $nFlag -ne 0 ]; then
-    if [ $# -lt 3 ] || [ $# -gt 4 ]; then
-        echo $USAGE
+fileName=${params[-1]}
+test -f $fileName
+
+# Checking return value of test function
+if [ $? -eq 1 ]; then
+        echo "ERROR: File $fileName does not exist"
         exit 1
-	fi
+fi
+
+# Cleaning up in the parameter array
+unset params[$(($#-1))]
+params=("${params[@]}")
+
+# Checking whether -n flag is used
+printf "%s" $@ | grep -q "\-n"
+if [ $? -eq 0 ]; then
+	nFlag=1
 else
-    if [ $# -lt 2 ] || [ $# -gt 3 ]; then
-		echo $USAGE
+	nFlag=0
+fi
+
+
+# Checking whether -F or -r flag is used
+printf "%s" $@ | grep -q "\-F\|\-r"
+if [ $? -eq 0 ]; then
+	oddFlag=1
+else
+	oddFlag=0
+fi
+
+# Checking whether -e flag is used
+printf "%s" $@ | grep -q "\-e"
+if [ $? -eq 0 ]; then
+	eFlag=1
+
+	# Cleaning up in the parameter array
+	index=$#
+	index=$((index-2))
+	unset params[$index]
+	params=("${params[@]}")
+else
+	eFlag=0
+fi
+
+# If the -n parameter is present - check for N argument
+if [ $nFlag -eq 1 ]; then
+	for index in "${!params[@]}"; do
+		if [[ "${params[$index]}" = "-n" ]]; then
+			# Updating index to point at next element in array - after -n parameter (should be a number)
+				index=$((index+1))
+			# Storing result in numberOfResults
+			numberOfResults=$(echo ${params[$index]} | grep ^[1-9][0-9]*)
+			# Cleaning up in the parameter array
+			unset params[$index]
+			unset params[$((index-1))]
+			params=("${params[@]}")
+		fi
+	done
+
+	if [ -z "$numberOfResults" ]; then
+		echo "ERROR: Incorrect or missing argument N to -n parameter"
+		echo ${USAGE}
 		exit 1
 	fi
 fi
 
-for value in ${flags[@]}
+# Check for number of parameters - taking into considerations which flags are being used
+if [[ $eFlag -eq 1 && $nFlag -eq 1 && $# -lt 5  ]] || [[ $nFlag -eq 1 && $# -lt 4  ]] || [[ $eFlag -eq 1 && $# -lt 3  ]] || [ $# -eq 1 ]; then
+	echo "ERROR: Incorrect number of parameters used"
+	echo ${USAGE}
+	exit 1
+fi
+
+
+for param in ${params[@]}
 do
-    case $value in
-        -c)
-            var="awk '{print \$1}' $filename | sort | uniq -c | sort -k1 -n -r | awk '{print \$2 \"\t\"  \$1}'"
+    case $param in
+	-c)
+		var="awk '{print \$1}' $fileName | sort | uniq -c | sort -k1 -n -r | awk '{print \$2 \"\t\" \$1}'"
+	 	printResult $var
+	    ;;
+	-r)
+            var="awk '{print \$1 \"\t\" \$9}' $fileName | sort -k2 -n -r"
+            printResult $var
+	    ;;
+	-F)
+            var="grep '\ [45][0-9][0-9]\ ' $fileName | awk '{print \$9 \"\t\" \$1}' | sort -k1 -n -r"
             printResult $var
             ;;
-        
-        -r)
-            var="awk '{print \$1 \"\t\" \$9}' $filename | sort -k2 -n -r"
-            printResult $var
-            
-            ;;
-        -F)
-            var="grep "\ [4][0-9][0-9]\ " $filename | awk '{print \$9 \"\t\" \$1}'"
-            printResult $var
-
-            ;;
-        -2)
-            var="grep "\ 2[0-9][0-9]\ " $filename | awk '{print \$1}' | sort | uniq -c | sort -k1 -n -r | awk '{print \$2 \"\t\"  \$1}'"
+	-2)
+            var="grep '\ 2[0-9][0-9]\ ' $fileName | awk '{print \$1}' | sort | uniq -c | sort -k1 -n -r | awk '{print \$2 \"\t\"  \$1}'"
             printResult $var
             ;;
 
-        -t)
-            var="awk '{print \$1 \"\t\" \$10}' $filename | awk '{array[\$1]+=\$2} END { for (i in array) {print i \"\t\" array[i]}}' | sort -k2 -r -n"
-            printResult $var
-            ;;
-        *)
-            echo "unkown parameter"
+	-t)
+		var="awk '{print \$1 \"\t\" \$10}' $fileName | awk '{array[\$1]+=\$2} END { for (i in array) {print i \"\t\" array[i]}}' | sort -k2 -r -n"
+		printResult $var
+		;;
+	 *)
+            echo "ERROR: Unknown parameter $param used"
             echo ${USAGE}
             exit 1
             ;;
